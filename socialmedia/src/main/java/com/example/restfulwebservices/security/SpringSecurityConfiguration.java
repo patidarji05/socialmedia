fb4f67755b3d8685d9c3d0cc5bf827051a99c9c0
patidarji05 <pradeeppatidar9009@gmail.com>
2024-08-26 10:23:41 +0530

    Add spring security

--- socialmedia/src/main/java/com/example/restfulwebservices/security/SpringSecurityConfiguration.java
+++ socialmedia/src/main/java/com/example/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -1,9 +1,10 @@
 package com.example.restfulwebservices.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,18 +12,19 @@ public class SpringSecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
-//		1) All requests should be authenticated
-		http.authorizeHttpRequests(
-				auth -> auth.anyRequest().authenticated()
-				);
-		
-//		2) If a request is not authenticated, a web page is show
-		http.httpBasic(withDefaults());
 
-		//		3) CSRF --> POST, PUT
-		
-		http.csrf().disable();
+//		1) All requests should be authenticated
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+//		2) If a request is not authenticated, a web page is show
+		// http.formLogin(Customizer.withDefaults());
+		http.httpBasic(Customizer.withDefaults());
+
+//		3) CSRF --> POST, PUT
+		http.csrf(custom -> custom.disable());
+
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
 		return http.build();
 	}
 
